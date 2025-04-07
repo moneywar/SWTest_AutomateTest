774f4bd6bf219879b8d0f9ea069ac99455543844
moneywar <kong.deesamoot@gmail.com>
2025-04-07 21:29:02 +0700

    change

--- backend/utils.test.js
+++ backend/utils.test.js
@@ -1,7 +1,24 @@
-const {addNumber} = require('./utils.js');
+import { addNumber, fetchData, calculateTotal } from './utils';
 
-// Test Function
-test('adds two number', () => {
-  expect(addNumber(2,3)).toBe(5);
-})
+// Mock fetchData globally
+jest.mock('./utils', () => ({
+    ...jest.requireActual('./utils'), // Retain other functions
+    fetchData: jest.fn() // Mock fetchData only
+}));
 
+describe('addNumber()', () => {
+    test('should return the sum of the two numbers', () => {
+        expect(addNumber(10, 20)).toBe(30); // 20 + 10
+    });
+});
+
+describe('fetchData()', () => {
+    test('should throw an error when called', () => {
+        // Mock fetchData to throw an error explicitly
+        fetchData.mockImplementationOnce(() => {
+            throw new Error('fetchData is not implemented yet');
+        });
+
+        expect(() => fetchData()).toThrow('fetchData is not implemented yet');
+    });
+});
